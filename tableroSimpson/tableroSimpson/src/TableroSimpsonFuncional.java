570c0609499a1bf82cb301067c9003512498343a
ibanezbetes <danizgz95@gmail.com>
2024-10-11 13:14:37 +0200

    [11/10/2024] 13:14

--- tableroSimpson/tableroSimpson/src/TableroSimpsonFuncional.java
+++ tableroSimpson/tableroSimpson/src/TableroSimpsonFuncional.java
@@ -34,15 +34,8 @@ public class TableroSimpsonFuncional {
         tablero[filaAleatorio][columnaAleatorio] = 'B';
 
         //5º IMPRIMIR TABLERO
-        for (int i = 0; i < MAX_FILA_TABLERO; i++) {
-            for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
-                //[0][0], [0][1],[0][2], ETC...
-                System.out.print(tablero[i][j] + " ");
-            }
-            System.out.println("");
-        }
-        System.out.println("");
-        System.out.println("");
+        imprimirTablero();
+        
 
         //6º ASIGNAR LOS 10 HOMER
         int filaAleatorioHomer;
@@ -52,14 +45,26 @@ public class TableroSimpsonFuncional {
                 filaAleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO); //De 0 a N-1
                 columnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO); //De 0 a N-1
             }while (tablero[filaAleatorioHomer][columnaAleatorioHomer]!='L');
-            //esto lo ejecutará cuando la casilla sea = L (pondrá H en la casilla)
+            //esto de abajo lo ejecutará cuando la casilla sea = L (pondrá H en la casilla)
             tablero[filaAleatorioHomer][columnaAleatorioHomer] = 'H';
-
         }
-        System.out.println("");
-        System.out.println("");
 
+        int filaAleatorioMuro;
+        int columnaAleatorioMuro;
+        for (int i = 0; i < 10; i++) {
+            do { //esto lo hace mientras la casilla sea diferente a L
+                filaAleatorioMuro = aleatorio.nextInt(MAX_FILA_TABLERO); //De 0 a N-1
+                columnaAleatorioMuro = aleatorio.nextInt(MAX_COLUMNA_TABLERO); //De 0 a N-1
+            }while (tablero[filaAleatorioMuro][columnaAleatorioMuro]!='L');
+            //esto de abajo lo ejecutará cuando la casilla sea = L (pondrá M en la casilla)
+            tablero[filaAleatorioMuro][columnaAleatorioMuro] = 'M';
+        }       
         //7º IMPRIMIR TABLERO
+        imprimirTablero();
+        tablero[MAX_COLUMNA_TABLERO-1][MAX_COLUMNA_TABLERO-1] = 'F';
+        imprimirTablero();
+    }
+    private static void imprimirTablero(){
         for (int i = 0; i < MAX_FILA_TABLERO; i++) {
             for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
                 //[0][0], [0][1],[0][2], ETC...
@@ -67,5 +72,9 @@ public class TableroSimpsonFuncional {
             }
             System.out.println("");
         }
-    }
+            System.out.println("");
+            System.out.println("");
+            System.out.println("");
 }
+
+}   
