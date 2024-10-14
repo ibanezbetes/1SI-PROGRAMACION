743cf0b579ad3960aa9cf29f8e237e15f13158d2
ibanezbetes <danizgz95@gmail.com>
2024-10-14 03:23:25 +0200

    [14/10/2024] 3:22
    desplazamiento de las 4 direcciones con limite de tablero, 'H' resta una vida y 'M' no deja avanzar por ahí

--- tableroSimpson/tableroSimpson/src/TableroSimpsonFuncional.java
+++ tableroSimpson/tableroSimpson/src/TableroSimpsonFuncional.java
@@ -1,69 +1,13 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TableroSimpsonFuncional {
     private static final int MAX_FILA_TABLERO = 10;
     private static final int MAX_COLUMNA_TABLERO = 10;
     private static char[][] tablero;
-    public static void main(String[] args) {
-        //1º inicializo matriz tablero
-        tablero = new char[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
+    private static int filaBart;
+    private static int columnaBart;
 
-        //2º RELLENAR EL TABLERO 'L'
-        for (int i = 0; i < MAX_FILA_TABLERO; i++) {
-            for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
-                //[0][0], [0][1], [0][2], ETC...
-                tablero[i][j] = 'L';
-            }
-        }
-        //3º IMPRIMIR TABLERO
-        for (int i = 0; i < MAX_FILA_TABLERO; i++) {
-            for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
-                //[0][0], [0][1],[0][2], ETC...
-                System.out.print(tablero[i][j] + " ");
-            }
-            System.out.println("");
-        }
-        System.out.println("");
-        System.out.println("");
-
-        //4º ASIGNAR A BART
-        Random aleatorio = new Random();
-        int filaAleatorio = aleatorio.nextInt(MAX_FILA_TABLERO); //De 0 a N-1
-        int columnaAleatorio = aleatorio.nextInt(MAX_COLUMNA_TABLERO); //De 0 a N-1
-
-        tablero[filaAleatorio][columnaAleatorio] = 'B';
-
-        //5º IMPRIMIR TABLERO
-        imprimirTablero();
-        
-
-        //6º ASIGNAR LOS 10 HOMER
-        int filaAleatorioHomer;
-        int columnaAleatorioHomer;
-        for (int i = 0; i < 10; i++) {
-            do { //esto lo hace mientras la casilla sea diferente a L
-                filaAleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO); //De 0 a N-1
-                columnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO); //De 0 a N-1
-            }while (tablero[filaAleatorioHomer][columnaAleatorioHomer]!='L');
-            //esto de abajo lo ejecutará cuando la casilla sea = L (pondrá H en la casilla)
-            tablero[filaAleatorioHomer][columnaAleatorioHomer] = 'H';
-        }
-
-        int filaAleatorioMuro;
-        int columnaAleatorioMuro;
-        for (int i = 0; i < 10; i++) {
-            do { //esto lo hace mientras la casilla sea diferente a L
-                filaAleatorioMuro = aleatorio.nextInt(MAX_FILA_TABLERO); //De 0 a N-1
-                columnaAleatorioMuro = aleatorio.nextInt(MAX_COLUMNA_TABLERO); //De 0 a N-1
-            }while (tablero[filaAleatorioMuro][columnaAleatorioMuro]!='L');
-            //esto de abajo lo ejecutará cuando la casilla sea = L (pondrá M en la casilla)
-            tablero[filaAleatorioMuro][columnaAleatorioMuro] = 'M';
-        }       
-        //7º IMPRIMIR TABLERO
-        imprimirTablero();
-        tablero[MAX_COLUMNA_TABLERO-1][MAX_COLUMNA_TABLERO-1] = 'F';
-        imprimirTablero();
-    }
     private static void imprimirTablero(){
         for (int i = 0; i < MAX_FILA_TABLERO; i++) {
             for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
@@ -77,4 +21,204 @@ public class TableroSimpsonFuncional {
             System.out.println("");
 }
 
-}   
+    private static void asignarLibreACasillasLibres(char caracter){
+        for (int i = 0; i < MAX_FILA_TABLERO; i++) {
+            for (int j = 0; j < MAX_COLUMNA_TABLERO; j++) {
+            //[0][0], [0][1], [0][2], ETC...
+            tablero[i][j] = caracter;
+        }
+    }
+    }
+
+    private static void asignarPersonajesACasillasLibres(char caracter, int numRepeticiones){
+    //4º ASIGNAR A BART
+    Random aleatorio = new Random();
+        int filaAleatorio = -1;
+        int columnaAleatorio = -1;
+        for (int i = 0; i < numRepeticiones; i++) {
+            do { //esto lo hace mientras la casilla sea diferente a L
+                filaAleatorio = aleatorio.nextInt(MAX_FILA_TABLERO); //De 0 a N-1
+                columnaAleatorio = aleatorio.nextInt(MAX_COLUMNA_TABLERO); //De 0 a N-1
+            }while (tablero[filaAleatorio][columnaAleatorio]!='L');
+            tablero[filaAleatorio][columnaAleatorio] = caracter;
+        }
+        if(caracter=='B'){
+            filaBart = filaAleatorio;
+            columnaBart = columnaAleatorio;
+        }
+    }
+
+    
+
+//    private static void asignarCaracterACasillasLibres(char caracter){
+//        int filaAleatorioHomer;
+//        int columnaAleatorioHomer;
+//        //int filaAleatorioMuro;
+//        //int columnaAleatorioMuro;        
+//        Random aleatorio = new Random();
+//        for (int i = 0; i < 10; i++) {
+//      do { //esto lo hace mientras la casilla sea diferente a L
+//            filaAleatorioHomer = aleatorio.nextInt(MAX_FILA_TABLERO); //De 0 a N-1
+//              columnaAleatorioHomer = aleatorio.nextInt(MAX_COLUMNA_TABLERO); //De 0 a N-1
+//            }while (tablero[filaAleatorioHomer][columnaAleatorioHomer]!='L');
+//            //esto de abajo lo ejecutará cuando la casilla sea = L (pondrá H en la casilla)
+//            tablero[filaAleatorioHomer][columnaAleatorioHomer] = caracter;
+//        }
+//    }
+    
+    public static void main(String[] args) {
+        //1º inicializo matriz tablero
+        tablero = new char[MAX_FILA_TABLERO][MAX_COLUMNA_TABLERO];
+        asignarLibreACasillasLibres('L');
+        imprimirTablero();
+        tablero[MAX_FILA_TABLERO-1][MAX_COLUMNA_TABLERO-1] = 'F';
+        imprimirTablero();
+        asignarPersonajesACasillasLibres('B', 1);
+        imprimirTablero();
+        asignarPersonajesACasillasLibres('H', 10);
+        imprimirTablero();
+        asignarPersonajesACasillasLibres('M', 10);
+        imprimirTablero();
+
+        // Desplazamiento de Bart
+        // A - IZQUIERDA
+        // D - DERECHA
+        // W - ARRIBA
+        // S - ABAJO
+    Scanner lector = new Scanner(System.in);
+    //************************************ */
+    /*BLOQUE DE JUGAR */
+    //************************************ */
+    int vidas = 3;    
+    do {
+            System.out.println("JUGAR");
+            System.out.println("Introduce el desplazamiento");
+            System.out.println("A - IZQUIERDA, D - DERECHA, W - ARRIBA, S - ABAJO");
+            String desplazamiento = lector.nextLine();
+            System.out.println("Desplazamiento=" + desplazamiento);
+
+
+    switch (desplazamiento) {
+        case "A":
+            // A - IZQUIERDA
+            if((columnaBart - 1) >= 0){
+                columnaBart = columnaBart - 1; //YA ESTÁ EN LA CASILLA MINIMA DEL TABLERO, SE SALDRÍA DEL ARRAY
+                switch (tablero[filaBart][columnaBart]) {
+                    case 'H':
+                    vidas = vidas - 1;
+                    tablero[filaBart][columnaBart] = 'B';
+                    tablero[filaBart][columnaBart+1] = 'L';
+                    System.out.println("Has perdido una vida. Te quedan " + vidas + " vidas.");
+                    break;
+                    case 'L':
+                    tablero[filaBart][columnaBart] = 'B';
+                    tablero[filaBart][columnaBart+1] = 'L';
+                    break;
+                    case 'M':
+                    System.out.println("Hay un muro. No puedes avanzar por ahí.");
+                    columnaBart = columnaBart + 1;
+                    break;
+                    
+                    default:
+                }
+                
+                                
+            }else  {
+                System.out.println("No se puede ir a la izquierda.");
+            }            
+            break;
+        case "S":
+            // S - ABAJO 
+            if((filaBart + 1) <= 9){
+                filaBart = filaBart + 1; //YA ESTÁ EN LA CASILLA MINIMA DEL TABLERO, SE SALDRÍA DEL ARRAY
+                switch (tablero[filaBart][columnaBart]) {
+                    case 'H':
+                    vidas = vidas - 1;
+                    tablero[filaBart][columnaBart] = 'B';
+                    tablero[filaBart-1][columnaBart] = 'L';
+                    System.out.println("Has perdido una vida. Te quedan " + vidas + " vidas.");
+                    break;
+                    case 'L':
+                    tablero[filaBart][columnaBart] = 'B';
+                    tablero[filaBart-1][columnaBart] = 'L';
+                    break;
+                    case 'M':
+                    System.out.println("Hay un muro. No puedes avanzar por ahí.");
+                    filaBart = filaBart - 1;
+                    break;
+                    
+                    default:
+                }
+                
+                                
+            }else  {
+                System.out.println("No se puede ir abajo.");
+            }            
+            break;
+        case "D":
+            // D - DERECHA
+            if((columnaBart + 1) <= 9){
+                columnaBart = columnaBart + 1; //YA ESTÁ EN LA CASILLA MINIMA DEL TABLERO, SE SALDRÍA DEL ARRAY
+                switch (tablero[filaBart][columnaBart]) {
+                    case 'H':
+                    vidas = vidas - 1;
+                    tablero[filaBart][columnaBart] = 'B';
+                    tablero[filaBart][columnaBart-1] = 'L';
+                    System.out.println("Has perdido una vida. Te quedan " + vidas + " vidas.");
+                    break;
+                    case 'L':
+                    tablero[filaBart][columnaBart] = 'B';
+                    tablero[filaBart][columnaBart-1] = 'L';
+                    break;
+                    case 'M':
+                    System.out.println("Hay un muro. No puedes avanzar por ahí.");
+                    columnaBart = columnaBart - 1;
+                    break;
+                    
+                    default:
+                }
+                
+                                
+            }else  {
+                System.out.println("No se puede ir a la derecha.");
+            }            
+            break;
+        case "W":
+            // W - ARRIBA
+            if((filaBart - 1) >= 0){
+                filaBart = filaBart - 1; //YA ESTÁ EN LA CASILLA MINIMA DEL TABLERO, SE SALDRÍA DEL ARRAY
+                switch (tablero[filaBart][columnaBart]) {
+                    case 'H':
+                    vidas = vidas - 1;
+                    tablero[filaBart][columnaBart] = 'B';
+                    tablero[filaBart+1][columnaBart] = 'L';
+                    System.out.println("Has perdido una vida. Te quedan " + vidas + " vidas.");
+                    break;
+                    case 'L':
+                    tablero[filaBart][columnaBart] = 'B';
+                    tablero[filaBart+1][columnaBart] = 'L';
+                    break;
+                    case 'M':
+                    System.out.println("Hay un muro. No puedes avanzar por ahí.");
+                    filaBart = filaBart + 1;
+                    break;
+                    
+                    default:
+                }
+                
+                                
+            }else  {
+                System.out.println("No se puede ir arriba.");
+            }            
+            break;
+        default:
+            break;
+    }
+    imprimirTablero();
+    
+    
+    }while (vidas > 0);
+
+}
+
+}
