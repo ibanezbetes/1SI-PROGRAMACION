37bc46edb525b3462e6027fa6f68d18139103ed5
ibanezbetes <danizgz95@gmail.com>
2024-10-21 01:10:54 +0200

    [21/10/2024] 1:10

--- bingo/src/bingo.java
+++ bingo/src/bingo.java
@@ -5,25 +5,82 @@ public class bingo {
     static final int MAX_FILA = 3;
     static final int MAX_COLUMNA = 9;
     static int tablero[][] = new int[MAX_FILA][MAX_COLUMNA];
+    static int ganadores[] = new int[15];
     static int decenas = 0;
     static int numCarton = 0;
 
-    public static void main(String[] args) {
-        for (int i = 0; i < MAX_FILA; i++) {
-            decenas = 10;
-            for (int j = 0; j < MAX_COLUMNA; j++) {
-                tablero[i][j] = aleatorio.nextInt(10) + decenas;
-                decenas = decenas + 10;
+    public static void definirArray() {
+            boolean repetido;
+            int randomizado;
+            for (int i=0; i < MAX_FILA; i++) {
+                decenas = 10;
+                for (int j=0; j < MAX_COLUMNA; j++) {
+                    do {
+                        randomizado = aleatorio.nextInt(10) + decenas;
+                    } while (comprobarRepetido(randomizado));  // Comprobamos si ya está en el array
+                    tablero[i][j] = randomizado;
+                    decenas = decenas + 10;
+                }
             }
+    }
+
+    public static void definirArrayGanadora() {
+        boolean repetido;
+        int randomizado;
+        for (int i=0; i < 15; i++) {
+                do {
+                    randomizado = aleatorio.nextInt(100);
+                } while (comprobarRepetido(randomizado));  // Comprobamos si ya está en el array
+                ganadores[i] = randomizado;
         }
-        numCarton = aleatorio.nextInt(1000);
-        System.out.println("---CARTÓN DE BINGO Nº" + numCarton + "--");
+    }
+
+    public static void generarNumCarton() {
+        numCarton =aleatorio.nextInt(900) + 100;
+        System.out.println("---CARTÓN DE BINGO Nº"+numCarton +"--");
         System.out.println("..........................");
-        for (int i = 0; i < MAX_FILA; i++) {
-            for (int j = 0; j < MAX_COLUMNA; j++) {
+    }
+
+    public static void imprimirArray() {
+        for (int i=0; i < MAX_FILA; i++) {
+            for (int j=0; j < MAX_COLUMNA; j++) {
                 System.out.print(tablero[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    public static void ordenarColumnas() {
+        for (int j = 0; j < MAX_COLUMNA; j++) {
+            // Aplicamos burbuja en cada columna
+            for (int i = 0; i < MAX_FILA - 1; i++) {
+                for (int k = i + 1; k < MAX_FILA; k++) {
+                    if (tablero[i][j] > tablero[k][j]) {
+                        // Intercambiamos si el valor de la fila superior es mayor que el inferior
+                        int numeroTemporal = tablero[i][j];
+                        tablero[i][j] = tablero[k][j];
+                        tablero[k][j] = numeroTemporal;
+                    }
+                }
+            }
+        }
+    }
+
+    public static boolean comprobarRepetido (int randomizado) {
+        for (int i = 0; i < MAX_FILA; i++) {
+            for (int j = 0; j < MAX_COLUMNA; j++) {
+                if (tablero[i][j] == randomizado) {
+                    return true;  // Si se encuentra el número, devolvemos true
+                }
+            }
+        }
+        return false;  // Si no se encuentra, devolvemos false
+    }
+
+    public static void main(String[] args) {
+        definirArray();
+        generarNumCarton();
+        ordenarColumnas();
+        imprimirArray();
     }
 }
